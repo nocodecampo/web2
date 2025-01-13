8b7440c3e48233354d6dc4e91fef8d3feaac259b
nocodecampo <dkampo@gmail.com>
2025-01-13 17:46:04 +0100

    enviar a BBDD

--- php/procesar-form.php
+++ php/procesar-form.php
@@ -1,6 +1,31 @@
+<!-- Conexion con BBDD -->
 <?php
-    if(isset($_POST['nombre'])) {
-        var_dump($_POST);
-        exit();
-    }
+// Configuración de la base de datos
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "web2";
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Comprobar conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+echo "Conexión exitosa - ";
+
+// Insertar datos en la base de datos
+$sql = "INSERT INTO usuarios (nombre, apellidos, email, fechaNac, password) VALUES ('" . $_POST['nombre'] . "', '" . $_POST['apellidos'] . "', '" . $_POST['email'] . "', '" . $_POST['fechaNac'] . "', '" . $_POST['password'] . "')";
+
+// Comprobar si se ha insertado correctamente
+if ($conn->query($sql) === TRUE) {
+    echo "Nuevo registro creado";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+// Cerrar conexión
+$conn->close();
+
 ?>
